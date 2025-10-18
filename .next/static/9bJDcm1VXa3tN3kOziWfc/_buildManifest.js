738c9d2a646e15515204ff5198f110b594bcf911
Joshua Idara <160030880+Joshua-ID@users.noreply.github.com>
2025-10-18 01:23:59 +0100

    Delete .next directory

--- .next/static/9bJDcm1VXa3tN3kOziWfc/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ee9ce975-9073181a1be7d6f5.js","static/chunks/pages/index-9f49ca5f036337ce.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
